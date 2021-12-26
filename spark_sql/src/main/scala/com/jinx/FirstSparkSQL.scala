8827da6972bc703bde3846ed4d8508d075311137
Jinx <1270621068qq.com>
2021-12-26 23:46:07 +0800

    1

--- spark_sql/src/main/scala/com/jinx/FirstSparkSQL.scala
+++ spark_sql/src/main/scala/com/jinx/FirstSparkSQL.scala
@@ -1,5 +1,7 @@
 package com.jinx
 
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -15,7 +17,17 @@ object FirstSparkSQL {
   def getNumOfGender(path:String)={
 
     val sc = new SparkContext(new SparkConf().setMaster("local").setAppName("getNumOfGender"))
+    val session = new SparkSession.Builder().appName("getNumOfGender")
     val userinfo = sc.textFile(path + "user.log")
+    val value = StructType("usrid::gender::age::occ::post".split("::").
+      map(x=>StructField(x,StringType,true)))
+    val usrRow = userinfo.map(_.split("::")).map(x => {
+      Row(x(0).trim,
+          x(1).trim,
+          x(2).trim,
+          x(3).trim,
+          x(4).trim)
+    })
 
 
   }
